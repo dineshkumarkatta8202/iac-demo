beefde540d9dbad18693da69a0ccf8c656b1502d
dineshkumarkatta8202 <137852839+dineshkumarkatta8202@users.noreply.github.com>
2023-07-26 09:36:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "s3_dinesh_26"
+  bucket = "dinesh26"
 
 
 }
 
 resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "s3_dinesh_26"
+  bucket = "dinesh26"
   key    = "vpc.tf"
   source = "${path.module}/vpc.tf"
   etag   = "${filemd5("${path.module}/vpc.tf")}"
